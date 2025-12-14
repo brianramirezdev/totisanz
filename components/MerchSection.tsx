1447167e194633bb33a198a21e03f32f9c26da80
Brian <brianramirezdelgadodev@gmail.com>
2025-12-14 14:01:29 +0000

    fix minimo de espaciado en merch

--- components/MerchSection.tsx
+++ components/MerchSection.tsx
@@ -13,7 +13,7 @@ const products = [
 
 export default function MerchSection() {
     return (
-        <section id="merch" className="bg-gray-50 px-6 py-8 md:py-16 m-4 rounded border border-gray-200">
+        <section id="merch" className="bg-gray-50 px-6 py-8 md:py-16 mx-4 rounded border border-gray-200">
             <div className="mx-auto max-w-7xl">
                 <div className="mb-8 md:mb-20 flex flex-col items-start">
                     <h2 className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl font-bold tracking-tight">MERCH </h2>
