4ca870e8ac9a64a556d22a32cebaa7226bd6db57
Brian <brianramirezdelgadodev@gmail.com>
2025-12-15 20:58:41 +0000

    actualizados cambios de SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,26 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = 'https://www.totisanz.com';
+
     return [
         {
-            url: 'https://www.totisanz.com/',
+            url: `${baseUrl}/`,
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 1,
+        },
+        {
+            url: `${baseUrl}/privacy`,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
+        },
+        {
+            url: `${baseUrl}/terms`,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
         },
     ];
 }
