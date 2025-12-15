c6a78dc99f84959cd05f2960892c4584ba7ca3ad
Brian <brianramirezdelgadodev@gmail.com>
2025-12-15 19:47:23 +0000

    ajuste www. en el sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://totisanz.com/',
+            url: 'https://www.totisanz.com/',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 1,
