08a203bb9bc278e1c9a56d89b30624e15ee6f7db
Brian <brianramirezdelgadodev@gmail.com>
2025-12-16 16:08:56 +0000

    fix favicon 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
 
     icons: {
         icon: [
-            // { url: '/favicon.ico', type: 'image/x-icon' },
+            { url: '/favicon.ico', type: 'image/x-icon' },
             { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
         ],
         apple: '/apple-icon-180x180.png',
